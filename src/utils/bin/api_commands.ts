a05623ce7360ee27a21d3aa168bb4b6df9cf402f
Terence Yu <diablodadayu@gmail.com>
2025-01-24 22:45:35 -0500

    customising

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -26,11 +26,11 @@ export const readme = async (args: string[]): Promise<string> => {
   ${readme}`;
 };
 
-export const weather = async (args: string[]): Promise<string> => {
-  const city = args.join('+');
-  if (!city) {
-    return 'Usage: weather [city]. Example: weather casablanca';
-  }
-  const weather = await getWeather(city);
-  return weather;
-};
+// export const weather = async (args: string[]): Promise<string> => {
+//   const city = args.join('+');
+//   if (!city) {
+//     return 'Usage: weather [city]. Example: weather casablanca';
+//   }
+//   const weather = await getWeather(city);
+//   return weather;
+// };
